53fc58f10dbc8d46616b3397d291bab4a8f739af
Beatriz Jacinto de Almeida <106273381+profBia@users.noreply.github.com>
2024-08-01 19:12:00 -0300

    .

--- script.js
+++ script.js
@@ -6,26 +6,19 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "A inteligência artificial (IA) vai revolucionar positivamente diversas indústrias?",
+        enunciado: "Pergunta 1",
         alternativas: [
-            "Alternativa 1",
+            "Alternativa 1", 
             "Alternativa 2"
         ]
     },
     {
-        enunciado: "A inteligência artificial pode ser uma ferramenta ética e responsável?",
+        enunciado: "Pergunta 2",
         alternativas: [
-            "Alternativa 1",
+            "Alternativa 1", 
             "Alternativa 2"
         ]
-    },
-    {
-        enunciado: "A inteligência artificial vai superar a inteligência humana em todos os aspectos?",
-        alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-        ]
-        },
+    } 
 ];
 
 let atual = 0;
@@ -33,9 +26,7 @@ let perguntaAtual;
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    }
-    
-    mostraPergunta();
-    
+    caixaPerguntas.textContent = perguntas[atual].enunciado;
+}
 
+mostraPergunta();
